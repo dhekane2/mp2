9b6155c23bcc9614f2363ade0cce9927f1460618
dhekane2 <dhekane2@illinois.edu>
2025-10-03 19:31:07 -0500

    added filter buttons in gallary_view

--- src/components/GalleryView.tsx
+++ src/components/GalleryView.tsx
@@ -1,10 +1,45 @@
 import React from "react";
+import styles from "../styles/FilterView.module.css";
+import {useState} from "react";
 
+
+const genreList = ["All", "Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Romance", "Sci-Fi", "Thriller"];
+
+function FilterSwitch() {
+
+    const [activeGenre, setActiveGenre] = useState("All");
+
+    return (
+        
+        <div className={styles.filters}>
+            {genreList.map((genre) => (
+                <button
+                    key = {genre}
+                    className = {`${styles.filterButton} ${activeGenre === genre ? styles.active : ""}`}
+                    onClick = {() => setActiveGenre(genre)}
+                >
+                    {genre}
+                </button>
+
+                
+            )) 
+            }
+            
+        </div>
+    )
+}
 
 function GalleryView() {
         return (
-        <div>
-            GalleryView
+        <div className={styles.container}>
+            <FilterSwitch />
+            
+            <div className={styles.gallery}>
+                {/* Gallery items would be rendered here */}
+                <p>Gallery items will be displayed here.</p>
+
+            </div>
+            
         </div>
     );
 }
